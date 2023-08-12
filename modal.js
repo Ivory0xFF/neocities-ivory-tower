dbcf10b3007d156e70b22961d7e7c929c8c11467
no <no>
2023-08-12 22:43:15 +0300

    updated tools.html :3

--- modal.js
+++ modal.js
@@ -1,5 +1,7 @@
 let modal=document.getElementsByClassName("modal")[0];
 let showcases=document.getElementsByClassName("showcase");
+let img=document.getElementsByClassName("img0")[0]
+let caption=document.getElementsByClassName("caption")[0]
 var close=document.getElementsByClassName("close")[0];
 for(var i=0;i<showcases.length;i++){
 var imgs=showcases[i].getElementsByTagName("img");
@@ -7,10 +9,10 @@ for(var x=0;x<imgs.length;x++){
 imgs[x].setAttribute("tabindex","0");
 imgs[x].setAttribute("loading","lazy");
 imgs[x].onclick=function(){modal.style.display="block";
-document.getElementsByClassName("img0")[0].src=this.src;
-document.getElementsByClassName("caption")[0].innerHTML=this.alt;};
+img.src=this.src;
+caption.innerHTML=this.alt;};
 imgs[x].onkeydown=function(k){if(k.key=="Enter"){modal.style.display="block";
-document.getElementsByClassName("img0")[0].src=this.src;
-document.getElementsByClassName("caption")[0].innerHTML=this.alt;}}}}
+img.src=this.src;
+caption.innerHTML=this.alt;}}}}
 close.onclick=function(){modal.style.display="none";};
 close.onkeydown=function(k){if(k.key=="Enter"){modal.style.display="none";}};
--- palettes.js
+++ palettes.js
@@ -1,22 +1,22 @@
 window.onload = function () {
     var p = document.getElementsByClassName("palette");
     for (i = 0; i < p.length; i++) {
-        var ctx = p[i].getContext("2d");
         var colors = p[i].getAttribute("alt").split(";");
-        p[i].width = colors.length * 64;
-        p[i].height = 64;
         for (a = 0; a < colors.length; a++) {
-            ctx.fillStyle = "#" + colors[a];
-            ctx.fillRect(a * 64, 0, 64, 64);
+            let div = document.createElement("div");
+            p[i].append(div);
             var m=colors[a].match(/.{2}/g);
-            console.log(m);
             let ihex="";
             for (l=0;l<m.length;l++){
                 if(m[l]!="ff"){ihex+=(255-parseInt(m[l],16)).toString(16);}else{ihex+="00"}
             }
-            ctx.fillStyle="#"+ihex;
-            ctx.font="32px Monospace";
-            ctx.fillText("WOW",a*64,64);
+            div.style.cssText = "background-color:#"+colors[a]+";color:#"+ihex;
+            div.className = "color";
+            div.innerText = colors[a];
+            div.setAttribute("onclick",`navigator.clipboard.writeText("${colors[a]}")`);
+            // div.addEventListener("onclick",navigator.clipboard.writeText(colors[a]));
+            // div.addEventListener("mouseente  r",()=>{div.innerText=`${window.getComputedStyle(div).backgroundColor}`});
+            // div.addEventListener("mouseleave",()=>{div.innerText=""});
         }
     }
 };
