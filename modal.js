fa999aa04cae14e7bd446b86f3bceed7fbda0b00
neh <neh>
2023-09-04 13:40:53 +0300

    three js stuff

--- modal.js
+++ modal.js
@@ -1,18 +1,18 @@
-let modal=document.getElementsByClassName("modal")[0];
+let modal=document.getElementById("modal");
+let img=document.getElementById("img0");
+let caption=document.getElementById("caption");
+var close=document.getElementById("close");
 let showcases=document.getElementsByClassName("showcase");
-let img=document.getElementsByClassName("img0")[0]
-let caption=document.getElementsByClassName("caption")[0]
-var close=document.getElementsByClassName("close")[0];
 for(var i=0;i<showcases.length;i++){
 var imgs=showcases[i].getElementsByTagName("img");
 for(var x=0;x<imgs.length;x++){
 imgs[x].setAttribute("tabindex","0");
 imgs[x].setAttribute("loading","lazy");
-imgs[x].onclick=function(){modal.style.display="block";
-img.src=this.src;
-caption.innerHTML=this.alt;};
-imgs[x].onkeydown=function(k){if(k.key=="Enter"){modal.style.display="block";
-img.src=this.src;
-caption.innerHTML=this.alt;}}}}
+imgs[x].onclick=function(){img.src=this.src;
+caption.innerText=this.alt;
+modal.style.display="block";};
+imgs[x].onkeydown=function(k){if(k.key=="Enter"){img.src=this.src;
+caption.innerText=this.alt;
+modal.style.display="block";}}}}
 close.onclick=function(){modal.style.display="none";};
 close.onkeydown=function(k){if(k.key=="Enter"){modal.style.display="none";}};
--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,21 @@
-import * as THREE from"https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-import{OrbitControls}from"https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-import{GLTFLoader}from"https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from"three";
+import{OrbitControls}from"three/addons/controls/OrbitControls.js";
+import{GLTFLoader}from"three/addons/loaders/GLTFLoader.js";
 function create_viewport(mesh,col){
 const gltfloader=new GLTFLoader();
 const scene=new THREE.Scene();
 const camera=new THREE.PerspectiveCamera(50,1,0.1,99);
 const renderer=new THREE.WebGLRenderer();
-const light=new THREE.PointLight(0xffffff);
-const amblight=new THREE.AmbientLight(0xDDDDDD);
+const light=new THREE.PointLight(0xffffff,2);
 new OrbitControls(camera,renderer.domElement);
-light.position.set(0,100,50);
-renderer.setClearColor(col,1);
-renderer.setSize(128,128);
-renderer.outputEncoding=THREE.sRGBEncoding;
-renderer.gammaFactor=2.2;
-renderer.domElement.style.cssText="";
+light.position.set(0,2,0);
+renderer.setClearColor(col,1);renderer.setSize(128,128);renderer.domElement.style.cssText="";
+document.getElementById(mesh).appendChild(renderer.domElement);
 camera.position.z=5;
 scene.add(light);
-scene.add(amblight);
-document.getElementById(mesh).appendChild(renderer.domElement);
+scene.add(new THREE.AmbientLight(0xffffff,2));
 gltfloader.load(`/${mesh}.gltf`,function(gltf){scene.add(gltf.scene);setInterval(function(){gltf.scene.rotation.y+=.01},10);});
-var render=function(){requestAnimationFrame(render);renderer.render(scene,camera);};
+function render(){requestAnimationFrame(render);renderer.render(scene,camera);}
 render();}
-create_viewport("Banan",0xCCCCFF);
+create_viewport("Banan",0xbbccff);
 create_viewport("BobaTea",0x5b224e);
