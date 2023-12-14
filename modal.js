e9dd61bc3c1b50a4b9f15f55205a5a87cf48a800
it me <88780282+Ivory0xFF@users.noreply.github.com>
2023-12-14 19:52:53 +0200

    Cool update

--- modal.js
+++ modal.js
@@ -2,26 +2,24 @@ let modal = document.getElementById("modal");
 let img = document.getElementById("img0");
 let caption = document.getElementById("caption");
 let x = document.getElementById("close");
-let showcases = document.getElementsByClassName("showcase");
-for (var i = 0; i < showcases.length; i++) {
-    var imgs = showcases[i].getElementsByTagName("img");
-    for (var z = 0; z < imgs.length; z++) {
-        imgs[z].tabIndex = "0";
-        imgs[z].loading = "lazy";
-        imgs[z].onclick = function () {
-            img.src = this.src;
-            caption.innerText = this.alt;
-            modal.style.display = "flex";
-        };
-        imgs[z].onkeydown = function (k) {
-            if (k.key == "Enter") {
-                img.src = this.src;
-                caption.innerText = this.alt;
-                modal.style.display = "flex";
-            }
+let imgs = document.querySelectorAll(".showcase img");
+
+for (var i = 0; i < imgs.length; i++) {
+    let c = imgs[i];
+    c.tabIndex = "0";
+    c.onclick = function () {
+        img.src = c.src;
+        caption.innerText = c.alt;
+        modal.style.display = "flex";
+        (c.classList.contains("pixelart")) ? img.className = "pixelart" : img.className = "";    
+    };
+    c.addEventListener("keydown", function onEvent(k) {
+        if (k.key === "Enter") {
+            c.click();
         }
-    }
+    });
 }
+
 x.onclick = function () {
     modal.style.display = "none";
 };
