a225a01c2835f059d94d6523f58e5d3f2c08371f
it me <88780282+Ivory0xFF@users.noreply.github.com>
2024-03-19 20:44:17 +0200

    better anchor links for portfolio.html

--- modal.js
+++ modal.js
@@ -6,12 +6,17 @@ let imgs = document.querySelectorAll(".showcase img");
 for (var i = 0; i < imgs.length; i++) {
     let c = imgs[i];
     c.tabIndex = "0";
+    c.id = c.alt.replace(/ |, /g, "-");
     c.onclick = function () {
         img.src = c.src;
         caption.innerHTML = c.alt;
         modal.style.display = "flex";
+        window.location.hash = c.id;
         (c.parentElement.classList.contains("pixelart")) || c.classList.contains("pixelart") ? img.className = "pixelart" : img.className = ""; 
     };
+    if ("#" + c.id == window.location.hash) {
+        c.click();
+    }
     c.addEventListener("keydown", function onEvent(k) {
         if (k.key === "Enter") {
             c.click();
