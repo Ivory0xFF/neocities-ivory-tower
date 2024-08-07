9056a470095acdb60340fd194eab4889b7a22d4a
it me <88780282+Ivory0xFF@users.noreply.github.com>
2024-08-07 13:48:07 +0300

    new update real

--- modal.js
+++ modal.js
@@ -4,57 +4,57 @@ let caption = document.getElementById("caption");
 let imgs = document.querySelectorAll(".showcase img");
 
 for (var i = 0; i < imgs.length; i++) {
-    let c = imgs[i];
-    c.tabIndex = "0";
-    c.id = c.alt.split(",")[0].replace(/ |, /g, "-");
-    c.onclick = function () {
-        img.src = c.src;
-        caption.innerHTML = c.alt;
-        modal.style.animation = "";
-        modal.style.display = "flex";
-        location.hash = c.id;
-        (c.closest(".showcase").classList.contains("pixelart")) || c.classList.contains("pixelart") ? img.className = "pixelart" : img.className = ""; 
-    };
-    if ("#" + c.id == window.location.hash) {
-        c.click();
-    }
-    c.addEventListener("keydown", k => {
-        if (k.key == "Enter") {
-            c.click();
-        }
-    });
+		let c = imgs[i];
+		c.tabIndex = "0";
+		c.id = c.alt.split(",")[0].replace(/ |, /g, "-");
+		c.onclick = function () {
+				img.src = c.src;
+				caption.innerHTML = c.alt;
+				modal.style.animation = "";
+				modal.style.display = "flex";
+				location.hash = c.id;
+				(c.closest(".showcase").classList.contains("pixelart")) || c.classList.contains("pixelart") ? img.className = "pixelart" : img.className = ""; 
+		};
+		if ("#" + c.id == window.location.hash) {
+				c.click();
+		}
+		c.addEventListener("keydown", k => {
+				if (k.key == "Enter") {
+						c.click();
+				}
+		});
 }
 
 window.addEventListener('hashchange', evt => {
-    let hash = window.location.hash;
-    if (hash) {
-        let query = document.querySelector(hash);
-        if (query.tagName == "IMG") {
-            query.click();
-        }
-    } else {
-        modal.style.display = "none";
-    }
+		let hash = window.location.hash;
+		if (hash) {
+				let query = document.querySelector(hash);
+				if (query.tagName == "IMG") {
+						query.click();
+				}
+		} else {
+				modal.style.display = "none";
+		}
 });
 
 modal.onclick = evt => {
-    if (!evt.target.closest("#img0bg")) {
-        h();
-    }
+		if (!evt.target.closest("#img0bg")) {
+				h();
+		}
 };
 
 window.addEventListener("keydown", evt => {
-    if (evt.key == "Escape") {
-        h();
-    }
+		if (evt.key == "Escape") {
+				h();
+		}
 });
 
 modal.addEventListener("animationend", evt => {
-    if (evt.animationName == "hide") {
-        modal.style.display = "none";
-    }
+		if (evt.animationName == "hide") {
+				modal.style.display = "none";
+		}
 });
 
 function h() { // Hide
-    modal.style.animation = "hide .2s forwards";
+		modal.style.animation = "hide .2s forwards";
 }
