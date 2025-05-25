86e1a54650c2998b4e7f49aeed8436a96504a943
it me <88780282+Ivory0xFF@users.noreply.github.com>
2025-05-25 22:14:35 +0300

    more styling

--- js/background.js
+++ js/background.js
@@ -2,12 +2,12 @@ import * as THREE from "three";
 import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 const gltfloader = new GLTFLoader();
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(50, 1, 0.1, 99);
+var camera = new THREE.PerspectiveCamera(70, 1, 0.1, 99);
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setClearColor(0xffffff, 0);
-renderer.setSize(window.innerWidth / 2, window.innerWidth / 2);
+renderer.setSize(window.innerWidth / 3, window.innerWidth / 3);
 document.body.appendChild(renderer.domElement);
-renderer.domElement.style.cssText = "z-index:-1;position:absolute;top:0%;width:100%";
+renderer.domElement.style.cssText = "z-index:-1;position:fixed;top:-12.5%;left:16.6%;width:66%;opacity:20%";
 camera.position.z = 4;
 let model;
 gltfloader.load("/ass/Banan.gltf", function (gltf) {
@@ -17,7 +17,7 @@ gltfloader.load("/ass/Banan.gltf", function (gltf) {
         if (o.isMesh) o.material = new THREE.MeshNormalMaterial({wireframe: true});
     });
     function render() {
-        model.rotation.y = Date.now() / 16000;
+        model.rotation.y = Date.now() / 32000;
         renderer.render(scene, camera);
         requestAnimationFrame(render);
     };
