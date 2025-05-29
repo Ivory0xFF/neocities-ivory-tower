be998f7a4281c434d9a4f1a5fa1f45b986117400
it me <88780282+Ivory0xFF@users.noreply.github.com>
2025-05-29 22:01:44 +0300

    big rework of index.html; more fixes

--- js/background.js
+++ js/background.js
@@ -2,20 +2,18 @@ import * as THREE from "three";
 import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 const gltfloader = new GLTFLoader();
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(70, 1, 0.1, 99);
+var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 99);
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setClearColor(0xffffff, 0);
-renderer.setSize(window.innerWidth / 3, window.innerWidth / 3);
+renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
 document.body.appendChild(renderer.domElement);
-renderer.domElement.style.cssText = "z-index:-1;position:fixed;top:-12.5%;left:16.6%;width:66%;opacity:20%";
-camera.position.z = 4;
+renderer.domElement.style.cssText = "z-index:-1;position:fixed;top:0;width:100%";
+camera.position.z = 3;
+camera.position.y = .8;
 let model;
-gltfloader.load("/ass/Banan.gltf", function (gltf) {
+gltfloader.load("/ass/muma/muma.glb", function (gltf) {
     model = gltf.scene
     scene.add(model);
-    model.traverse((o) => {
-        if (o.isMesh) o.material = new THREE.MeshNormalMaterial({wireframe: true});
-    });
     function render() {
         model.rotation.y = Date.now() / 32000;
         renderer.render(scene, camera);
