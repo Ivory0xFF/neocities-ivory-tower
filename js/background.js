9e2158068f97b1dac8959a285cdcc74b2ca52092
it me <88780282+Ivory0xFF@users.noreply.github.com>
2025-06-16 22:41:27 +0300

    animatin

--- js/background.js
+++ js/background.js
@@ -8,15 +8,20 @@ renderer.setClearColor(0xffffff, 0);
 renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
 document.body.appendChild(renderer.domElement);
 renderer.domElement.style.cssText = "z-index:-1;position:fixed;top:0;width:100%";
-camera.position.z = 3;
+camera.position.z = 2;
 camera.position.y = .8;
 let model;
 gltfloader.load("/ass/muma/muma.glb", function (gltf) {
     model = gltf.scene;
+
+    const mixer = new THREE.AnimationMixer(model);
+    mixer.clipAction(THREE.AnimationClip.findByName(gltf.animations, "idle")).play();
+
     scene.add(model);
     function render() {
         model.rotation.y = Date.now() / 32000;
         renderer.render(scene, camera);
+        mixer.update(Date.now());
         requestAnimationFrame(render);
     };
     render();
