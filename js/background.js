470111031cf4c2ff33d1902c693ce7febabe8790
it me <88780282+Ivory0xFF@users.noreply.github.com>
2025-06-19 19:02:50 +0300

    another index revamp; scrollbar

--- js/background.js
+++ js/background.js
@@ -5,7 +5,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 99);
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setClearColor(0xffffff, 0);
-renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+renderer.setSize(window.innerWidth / 1, window.innerHeight / 1);
 document.body.appendChild(renderer.domElement);
 renderer.domElement.style.cssText = "z-index:-1;position:fixed;top:0;width:100%";
 camera.position.z = 2;
