a410af0c983453c6ac2e38ecd605e3b4ccab01f7
Ivore <88780282+Ivory0xFF@users.noreply.github.com>
2025-09-21 15:13:42 +0300

    move camera based on scroll in index + removed some archaic code

--- js/background.js
+++ js/background.js
@@ -1,18 +1,25 @@
 import * as THREE from "three";
 import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
-const gltfloader = new GLTFLoader();
+
+var gltfloader = new GLTFLoader();
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 99);
-var renderer = new THREE.WebGLRenderer({alpha: true});
-renderer.setClearColor(0xffffff, 0);
-renderer.setSize(window.innerWidth / 1, window.innerHeight / 1);
+var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 9);
+var renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.domElement.style.cssText = "z-index:-1;position:fixed;top:0;width:100%";
-camera.position.z = 2;
-camera.position.y = .8;
-let model;
+
+function lerp(from, to, amount) {
+	return from * (1 - amount) + to * amount;
+}
+
+function getScroll() {
+	return window.scrollY / (document.body.scrollHeight - window.innerHeight);
+}
+
 gltfloader.load("/ass/muma/muma.glb", function (gltf) {
-    model = gltf.scene;
+    let model = gltf.scene;
 
     const mixer = new THREE.AnimationMixer(model);
     mixer.clipAction(THREE.AnimationClip.findByName(gltf.animations, "idle")).play();
@@ -20,10 +27,18 @@ gltfloader.load("/ass/muma/muma.glb", function (gltf) {
     scene.add(model);
 
     let clock = new THREE.Clock();
-    function render() {
+		let scroll = getScroll();
+		let lastScroll = scroll;
+
+		function render() {
         model.rotation.y = Date.now() / 32000;
+				scroll = lerp(lastScroll, getScroll(), 0.5 * clock.getDelta());
+				camera.rotation.x = 0 + (Math.PI / 32 * scroll);
+				camera.position.z = 2 - scroll * 1.4;
+				camera.position.y = 0.5 + scroll * 0.9;
+				lastScroll = scroll;
         renderer.render(scene, camera);
-        mixer.update(clock.getDelta()*0.66);
+        mixer.update(clock.getDelta() * 0.66);
         requestAnimationFrame(render);
     };
     render();
